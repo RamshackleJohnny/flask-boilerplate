5d494f5e153aa3d77631151de9431f6183348491
Jonathan Marciano <bobthemoon@gmail.com>
2019-03-20 13:37:50 -0400

    - Hello world now works - Hello world take 'name' parameter

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return 'This is a flask-boilerplate project, not to be used in production.'
 
+    @app.route('/hello')
+    def hello():
+        name = request.args.get('name', 'World')
+        return f"Hello {name}!"
+    
     return app
---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+from flask_boilerplate import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_index(client):
+    response = client.get('/')
+    assert response.data == b'This is a flask-boilerplate project, not to be used in production.'
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello World!'
+
+    response = client.get('/hello?name=Jim')
+    assert response.data == b'Hello Jim!'
